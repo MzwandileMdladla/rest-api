75ea22840798c9d3d194ccbdfbde6705fd2a69c0
Mzwandile Mdladla (ZA) <Mzwandile.Mdladla@absa.africa>
2020-03-15 19:01:00 +0200

    Adding Topics database Adding Flyway database migration

---
+++ src/main/resources/db/migration/postgresql/V1__topics_initial_create.sql
@@ -0,0 +1,6 @@
+create table topics (
+    id varchar(20) not null,
+    name varchar(20),
+    description varchar(100),
+    constraint topic_pkey primary key (id)
+);
---
+++ src/main/resources/db/migration/postgresql/V2__add_topics.sql
@@ -0,0 +1,3 @@
+insert into topics (id, name, description) values ('java', 'Core Java', 'Core Java Description');
+insert into topics (id, name, description) values ('spring', 'Spring Framework', 'Spring Framework Edition');
+insert into topics (id, name, description) values ('javascript', 'JavaScript', 'JavaScript Edition');
